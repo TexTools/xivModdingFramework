f0e621a7277a6fd7107a686de63283efa33fb541
Liinko <dualwielddevelopment@gmail.com>
2018-10-07 14:37:24 -0500

    Addition of generic item model

---
+++ xivModdingFramework/Items/DataContainers/XivGenericItemModel.cs
@@ -0,0 +1,58 @@
+﻿// xivModdingFramework
+// Copyright © 2018 Rafael Gonzalez - All Rights Reserved
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using xivModdingFramework.Items.Interfaces;
+
+namespace xivModdingFramework.Items.DataContainers
+{
+    /// <summary>
+    /// This class holds information for Generic Items with models
+    /// </summary>
+    public class XivGenericItemModel : IItemModel
+    {
+        /// <summary>
+        /// The name of the item
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Main Category
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// The item Category
+        /// </summary>
+        public string ItemCategory { get; set; }
+
+        /// <summary>
+        /// The item SubCategory
+        /// </summary>
+        public string ItemSubCategory { get; set; }
+
+        /// <summary>
+        /// The Model Information for the gear item
+        /// </summary>
+        public XivModelInfo ModelInfo { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return string.Compare(Name, ((XivGenericItemModel)obj).Name, StringComparison.Ordinal);
+        }
+
+    }
+}
