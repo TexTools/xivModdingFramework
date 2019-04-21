c1bc0f2fe1ddda72ca2aa575a71ddfdab8d468c6
Liinko <dualwielddevelopment@gmail.com>
2019-04-21 12:51:02 -0500

    Addition of Paintings

--- xivModdingFramework/Resources/XivStrings.Designer.cs
+++ xivModdingFramework/Resources/XivStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace xivModdingFramework.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class XivStrings {
@@ -1767,6 +1767,15 @@ namespace xivModdingFramework.Resources {
         internal static string Other {
             get {
                 return ResourceManager.GetString("Other", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Paintings.
+        /// </summary>
+        internal static string Paintings {
+            get {
+                return ResourceManager.GetString("Paintings", resourceCulture);
             }
         }
         
