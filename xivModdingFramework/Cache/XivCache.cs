9802472e056e8590fe7a44e670453d91a6ce25f9
Ivanka Heins <shinnova@users.noreply.github.com>
2020-08-12 13:15:21 +0200

    Ensure CreateRootCacheDB.sql can always be found

--- xivModdingFramework/Cache/XivCache.cs
+++ xivModdingFramework/Cache/XivCache.cs
@@ -350,7 +350,7 @@ namespace xivModdingFramework.Cache
                     using (var db = new SQLiteConnection(RootsCacheConnectionString))
                     {
                         db.Open();
-                        var lines = File.ReadAllLines(Path.Combine("Resources", "SQL", rootCacheCreationScript));
+                        var lines = File.ReadAllLines(Path.Combine(cwd, "Resources", "SQL", rootCacheCreationScript));
                         var sqlCmd = String.Join("\n", lines);
 
                         using (var cmd = new SQLiteCommand(sqlCmd, db))
@@ -1435,6 +1435,7 @@ namespace xivModdingFramework.Cache
 
         public static void ResetRootCache()
         {
+            var cwd = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
 
             SQLiteConnection.ClearAllPools();
             GC.WaitForPendingFinalizers();
@@ -1443,7 +1444,7 @@ namespace xivModdingFramework.Cache
             using (var db = new SQLiteConnection(RootsCacheConnectionString))
             {
                 db.Open();
-                var lines = File.ReadAllLines(Path.Combine("Resources", "SQL", rootCacheCreationScript));
+                var lines = File.ReadAllLines(Path.Combine(cwd, "Resources", "SQL", rootCacheCreationScript));
                 var sqlCmd = String.Join("\n", lines);
 
                 using (var cmd = new SQLiteCommand(sqlCmd, db))
