c89761ed2aae9456fc1a11e24a53b146d7b83300
Sel <admin@xivmodarchive.com>
2020-09-21 18:14:18 -0400

    - Fix for writing file additions to incorrect hashes.

--- xivModdingFramework/SqPack/DataContainers/IndexFile.cs
+++ xivModdingFramework/SqPack/DataContainers/IndexFile.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using xivModdingFramework.Exd.FileTypes;
 using xivModdingFramework.General.Enums;
 using xivModdingFramework.Helpers;
 
@@ -154,8 +155,10 @@ namespace xivModdingFramework.SqPack.DataContainers
                         {
                             Index1Entries.Add(entry.FolderPathHash, new Dictionary<uint, FileIndexEntry>());
                         }
-                        Index1Entries[entry.FolderPathHash].Add(entry.FileNameHash, entry);
-                    }
+                        if (!Index1Entries[entry.FolderPathHash].ContainsKey(entry.FileNameHash))
+                        {
+                            Index1Entries[entry.FolderPathHash].Add(entry.FileNameHash, entry);
+                        }
                 } else if(segmentId == 1 || segmentId == 2)
                 {
                     // Segment 4 is regenerated when writing, so we don't need to store it.
@@ -208,12 +211,16 @@ namespace xivModdingFramework.SqPack.DataContainers
 
                         var bytes = stream.ReadBytes(8);
                         entry.SetBytes(bytes);
-                        Index2Entries.Add(entry.FullPathHash, entry);
+
+                        if (!Index2Entries.ContainsKey(entry.FullPathHash))
+                        {
+                            Index2Entries.Add(entry.FullPathHash, entry);
+                        }
                     }
                 }
                 else if (segmentId == 1 || segmentId == 2 || segmentId == 3)
                 {
-                    Index2ExtraSegments.Add(stream.ReadBytes(segmentSize));
+                       Index2ExtraSegments.Add(stream.ReadBytes(segmentSize));
                 }
             }
 
@@ -253,6 +260,10 @@ namespace xivModdingFramework.SqPack.DataContainers
                     }
 
                     folderListing[folderKey].FileCount++;
+                    if (entry.FileNameHash != fileKey || entry.FolderPathHash != folderKey)
+                    {
+                        throw new Exception("Attempted to save Index file with invalid structure.");
+                    }
                     fileListing.Add(entry);
 
                     currentFileOffset += 16;
@@ -668,7 +679,7 @@ namespace xivModdingFramework.SqPack.DataContainers
                 if (newRawOffsetWithDatNumEmbed != 0)
                 {
                     var entry = new FileIndexEntry(newRawOffsetWithDatNumEmbed, fileHash, folderHash);
-                    Index1Entries[folderHash].Add(newRawOffsetWithDatNumEmbed, entry);
+                    Index1Entries[folderHash].Add(fileHash, entry);
                 }
             } else
             {
